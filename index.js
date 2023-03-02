19d25b66564dbb3a09519ada8697332f24e60a19
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-02 01:04:24 -0700

    Created array for inquirer prompt in index.js

--- index.js
+++ index.js
@@ -2,3 +2,34 @@ const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+//stores Employee Objects
+const employees = [];
+
+//prompts user for information
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of the employee?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?",
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What type of employee are they?",
+            choices: ["Engineer", "Intern", "Manager"],
+        },
+    ])
+}
+
